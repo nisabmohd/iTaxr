2d48cd430991dc6298c08939b859420298a2c026
Mohd. Nisab <nisabmohd@gmail.com>
2024-11-19 23:22:12 +0530

    made build issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,6 @@ const nextConfig = {
       bodySizeLimit: "3mb",
     },
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
